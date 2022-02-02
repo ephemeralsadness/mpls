5d061dde28de503c7c64172f2e6628e26f823585
braveoni <49433877+braveoni@users.noreply.github.com>
2022-02-02 22:39:15 +0300

    Frontend

--- src/static/linechart.js
+++ src/static/linechart.js
@@ -25,7 +25,9 @@ var getData = () => {
         url: '/plot',
         success: function(data) {
             line.data.labels = data.labels;
-            line.data.datasets = data.data;
+            line.data.datasets[0] = data.data;
+			line.data.datasets[0].backgroundColor = '#ff4747';
+			line.data.datasets[0].tension = 0.4;
         }
     })
 }
