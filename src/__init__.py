ce8ce114f8bb0ed35c1ca907c91d200114c224cf
Alexander Sarantcev <sarancev.dev@gmail.com>
2022-02-03 08:55:28 +1000

    Changed timebounds

--- src/__init__.py
+++ src/__init__.py
@@ -12,6 +12,6 @@ login_manager = LoginManager(app)
 
 
 from plot import Scheduler, convert
-s = Scheduler(timedelta(seconds=20)).add_job(convert, [timedelta(seconds=20)])
+s = Scheduler(timedelta(minutes=5)).add_job(convert, [timedelta(minutes=5)])
 
 from . import routes
