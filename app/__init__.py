927e2654c293d9efa22887083d35382a75ec370b
Alexander Sarantcev <sarancev.dev@gmail.com>
2022-02-03 02:52:32 +1000

    Created engine

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config.from_object('app.config.Config')
 
 db = SQLAlchemy(app)
+engine = create_engine('sqlite:////database.db')
 login_manager = LoginManager(app)
 
 from . import routes
