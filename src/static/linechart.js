4c69343341f33949693bdbda34537be38917ecdc
braveoni <49433877+braveoni@users.noreply.github.com>
2022-02-03 00:38:05 +0300

    Fix LineChart

--- src/static/linechart.js
+++ src/static/linechart.js
@@ -1,43 +1,55 @@
-var ctx = document.getElementById('line')
 
-var line = new Chart(ctx, {
-    type: 'line',
-	data: {
-		labels: [],
-		datasets: []
-	},
-	options: {
-		responsive: true,
-		title: {
-			display: false,
-			text: "TesT",
-		},
-		plugins: {
-      		title: {
-        		display: true,
-        		text: 'Test'
-      		},
-			legend: {
-				position: null,
-			}
-    	},
-	}
-})
 
 var getData = () => {
     $.ajax({
         url: '/plot',
         success: function(data) {
-            line.data.labels = data.test.labels;
-            line.data.datasets = data.test.data;
-			line.data.datasets[0].borderColor = '#ff4747';
-			line.data.datasets[0].backgroundColor = '#ff4747';
-			line.data.datasets[0].tension = 0.4;
-			line.data.datasets[0].fill = true;
+			var charts = document.getElementById('charts')
+			$.each(data, function(i, data) {
+				if ($('#' + i).length > 0) {
+					$('#' + i).remove();
+				}
+				
+				var canvas = document.createElement('canvas');
+				canvas.id = i;
+				charts.appendChild(canvas);
+				var ctx = document.getElementById(i).getContext("2d");
+				var line = new Chart(ctx, {
+					type: 'line',
+					data: {
+						labels: [],
+						datasets: []
+					},
+					options: {
+						responsive: true,
+						title: {
+							display: false,
+							text: "TesT",
+						},
+						plugins: {
+							title: {
+								display: true,
+								text: 'Test'
+							},
+							legend: {
+								position: null,
+							}
+						},
+					}
+				})
 
-			line.update();
+				line.data.labels = data.labels;
+				line.data.datasets = data.data;
+				line.data.datasets[0].borderColor = '#ff4747';
+				line.data.datasets[0].backgroundColor = '#ff4747';
+				line.data.datasets[0].tension = 0.4;
+				line.data.datasets[0].fill = true;
+
+				line.update();
+			})
         }
     })
 }
 
-getData()
+getData();
+setInterval(getData, 10000);
