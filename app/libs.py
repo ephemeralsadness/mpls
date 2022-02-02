a625fc486160a4d854adc9a071c1f50155e30965
Alexander Sarantcev <sarancev.dev@gmail.com>
2022-02-03 04:29:12 +1000

    Deleted unused functions from libs.py

--- app/libs.py
+++ app/libs.py
@@ -7,11 +7,3 @@ def round_down_datetime(dt: datetime, delta: timedelta):
 
 def round_up_datetime(dt: datetime, delta: timedelta):
     return round_down_datetime(dt, delta) + delta
-
-
-def get_last_table(delta):
-    return round_down_datetime(datetime.now(), delta).strftime('%Y-%m-%dT%H:%M')
-
-
-def get_next_table(delta):
-    return round_up_datetime(datetime.now(), delta).strftime('%Y-%m-%dT%H:%M')
